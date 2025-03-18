fb4b47f08944146200aef4d03e8d590b7eff5f44
anshul <anshulagr799@gmail.com>
2025-03-18 14:46:06 +0530

    add remote_base_url and local_base_url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 
-export const remote_base_url = 'https://school-enquiry-backend.onrender.com/';
+export const remote_base_url = 'https://school-enquiry-backend.onrender.com';
 
-export const local_base_url = 'http://localhost:4000/';
+export const local_base_url = 'http://localhost:4000';
 
 // const base_url = process.env.NODE_ENV === 'development' ? local_base_url : remote_base_url  ;
